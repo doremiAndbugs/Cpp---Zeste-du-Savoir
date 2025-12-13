29c42508897a92dc1720835e9663bb6be4986b8d
amira-abi <3camiraadjabi@gmail.com>
2025-12-13 23:49:23 +0100

    I finished the askPersonalInformations.cpp file , It supposed to ask the name, lastname, gender and age and put those informations in a csv file that is name after the lastname and name that wer enter (lastname.name.csv)

--- part2/nouvelleStructures/askPersonalInformation.cpp
+++ part2/nouvelleStructures/askPersonalInformation.cpp
@@ -1,19 +1,57 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <limits>
+
+template<typename T>
+void secureInput(T & variable){
+    while (!(std::cin >> variable)){
+        std::cout << "Entrée invalide. Recommence." << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+}
+template<typename T, typename Predicate>
+void secureInput(T & variable, Predicate predicate){
+    while (!(std::cin >> variable) || !predicate(variable) ){
+        std::cout << "Entrée invalide. Recommence." << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+}
+
+void askInformations(std::string& name, std::string& lastname, std::string& gender, int& age){
+    std::cout << "NOM ?" << std::endl;
+    secureInput(name);
+
+    std::cout << "PRENOM ?" << std::endl;
+    secureInput(lastname);
+
+    std::cout << "SEXE ?" << std::endl;
+    secureInput(gender);
+    std::cout << "AGE ?" << std::endl;
+    secureInput(age, [](int a){return a >=0;});
+}
+
+std::string registerInformations(std::string const name, std::string const lastname, std::string const gender, int const age){
+    std::string fileName { name +"."+lastname+".csv"};
+    std::ofstream file {fileName};
+    file << name <<", "<< lastname << ", " << gender << ", " << age;
+    return fileName;
+}
 
 int main(){
 
-    std::vector<std::string> ask {"NOM ?", "PRENOM ?", "AGE ?", "SEXE ?"}
-    std::ofstream file {"Cameron.Diaz.csv"};
+    std::string name {""};
+    std::string lastname {""};
+    std::string gender {""};
+    int age { 0 };
 
-    std::string input {""};
+    askInformations(name, lastname, gender, age);
+    auto fileName = registerInformations(name, lastname, gender, age);
 
-    for( question : ask) {
-        std::cout << question << std::endl;
-        std::cin >> input;
-        file << std::flush;
-    }
-
-    std::cout << "Thos info will be saved in the file Cameron.Diaz.csv" << std::endl;
+    std::cout << "Those info will be saved in the file " << fileName << std::endl;
 
     return 0;
 
