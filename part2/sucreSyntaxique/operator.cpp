0abe5467d5a0202cfdd1295fd1bd00f582104f05
amira-abi <3camiraadjabi@gmail.com>
2026-01-24 15:07:20 +0100

    new Chapter

---
+++ part2/sucreSyntaxique/operator.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+struct Fraction
+{
+    int numerateur;
+    int denominateur;
+};
+
+// Ça ressemble à quelque chose de connu, hein ?
+Fraction operator+(Fraction const & a, Fraction const & b)
+{
+    int numerateur { a.numerateur * b.denominateur + b.numerateur * a.denominateur };
+    int denominateur { a.denominateur * b.denominateur };
+
+    return { numerateur, denominateur };
+}
+
+Fraction operator-(Fraction const & a, Fraction const & b)
+{
+    int numerateur { a.numerateur * b.denominateur - b.numerateur * a.denominateur };
+    int denominateur { a.denominateur * b.denominateur };
+
+    return { numerateur, denominateur };
+}
+
+Fraction operator/(Fraction const & a, Fraction const & b)
+{
+  int numerateur { a.numerateur * b.denominateur };
+  int denominateur { a.denominateur * b.numerateur};
+}
+
+
+int main()
+{
+    Fraction const un_demi { 1, 2 };
+    Fraction const trois_quarts { 3, 4 };
+    Fraction const deux_huitièmes { 2, 8 };
+
+    Fraction const resultat { un_demi + trois_quarts - deux_huitièmes };
+    std::cout << "1/2 + 3/4 - 2/8 font " << resultat.numerateur << "/" << resultat.denominateur << std::endl;
+
+    return 0;
+}
