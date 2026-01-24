7c4573a6896b2f65890d11adb33a3ea5ba5974d2
amira-abi <3camiraadjabi@gmail.com>
2026-01-24 16:32:08 +0100

    Added toSEconds() function

--- part2/sucreSyntaxique/duration.cpp
+++ part2/sucreSyntaxique/duration.cpp
@@ -6,16 +6,22 @@ struct Duration
   int seconds;
 };
 
-std::tuple<int> secondToMinutes(Duration & s){
+int toSeconds(){
+  return seconds;
+}
+
+std::tuple<int,int> toMinutes(){
   int minutes=0;
   if(s<60){
-    int duration= std::tuple {0, s.seconds};
+    int duration= std::tuple {0, seconds};
   }else{
     while(s>60){
-    s.seconds = s.seconds-60;
+    s.seconds = seconds-60;
     minutes++;
     }
     int duration = std::tuple {minutes, s.seconds};
   }
   return duration;
 }
+
+std::tuple<int,int,int> toHours(){}
