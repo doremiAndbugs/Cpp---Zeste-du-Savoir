1a50acc4505ecdeffed7d5780d709bebdba4e190
amira-abi <3camiraadjabi@gmail.com>
2026-01-24 16:33:26 +0100

    Changes of the toMinutes functions

--- part2/sucreSyntaxique/duration.cpp
+++ part2/sucreSyntaxique/duration.cpp
@@ -15,10 +15,8 @@ std::tuple<int,int> toMinutes(){
   if(s<60){
     int duration= std::tuple {0, seconds};
   }else{
-    while(s>60){
-    s.seconds = seconds-60;
-    minutes++;
-    }
+    minutes = seconds/60;
+    seconds = seconds%60;
     int duration = std::tuple {minutes, s.seconds};
   }
   return duration;
