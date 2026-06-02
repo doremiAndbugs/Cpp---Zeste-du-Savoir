3f1ddf03620bdd2fceb87ef43f9eb5125a8bd856
doremi <171094069+doremiAndbugs@users.noreply.github.com>
2026-06-02 22:50:38 +0100

    added the prototypes of a few tests for addSong, the file is not perfect but i'm trying to learn how to make test in C++, so it's really new to me

---
+++ Discographie/test.cpp
@@ -0,0 +1,21 @@
+#include "discography.h"
+
+void addSongAddUnknownIf0Parameters(){};
+
+void addSongAddUnknownIfAlbumNotGiven(){};
+
+void addSongAddUnknownIfSongNotGiven(){};
+
+void addSongAddUnknownIfSingerNotGiven(){};
+
+void addSong_addNothingIfSongExistAlready(){};
+
+void addSong_addNewSongToAnExistentAlbum(){};
+
+void addSong_addNewAlbumToAnExistentSinger(){};
+
+int maint(){
+  return 0;
+
+}
+
