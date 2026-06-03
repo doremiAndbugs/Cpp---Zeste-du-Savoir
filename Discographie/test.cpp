026b23ca698a8d9b2c3e5d8c6c3d19cdaa3b121d
doremi <171094069+doremiAndbugs@users.noreply.github.com>
2026-06-03 16:45:32 +0100

    added addSongAddUnknownIf0Parameters() in test.cpp

--- Discographie/test.cpp
+++ Discographie/test.cpp
@@ -1,6 +1,17 @@
 #include "discography.h"
+#include <cassert>
 
-void addSongAddUnknownIf0Parameters(){};
+/**
+ *This functions verify that if there is no given parameters,
+ *it will replace everything by "unknown" and will add "Unknwon | Unknown |Unknown"
+ */
+void addSongAddUnknownIf0Parameters(){
+  Song song = {"inconnu"};
+  Album album = {"inconnu", song};
+  Artiste artiste = {"inconnu", album};
+  addSong(discography);
+  assert(isItHere(discography, artiste)=!-1);
+};
 
 void addSongAddUnknownIfAlbumNotGiven(){};
 
